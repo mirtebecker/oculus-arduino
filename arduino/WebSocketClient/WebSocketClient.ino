6775c386f96729359db52810584109fb0b0bafc0
mirtebecker <mailmirte@gmail.com>
2014-05-05 18:00:13 -0400

    so much cool stuff

--- arduino/WebSocketClient/WebSocketClient.ino
+++ arduino/WebSocketClient/WebSocketClient.ino
@@ -1,16 +1,3 @@
-/*
- * WiFlyHQ Example WebSocketClient.ino
- *
- * This sketch implements a simple websocket client that connects to a 
- * websocket echo server, sends a message, and receives the response.
- * Accepts a line of text via the serial monitor, sends it to the websocket
- * echo server, and receives the echo response.
- * See http://www.websocket.org for more information.
- *
- * This sketch is released to the public domain.
- *
- */
-
 #include "Arduino.h"
 #include <WiFlyHQ.h>
 //#include <SoftwareSerial.h>
@@ -30,33 +17,67 @@ bool connect(const char *hostname, const char *path="/", uint16_t port=80);
 //SoftwareSerial wifiSerial(8,9);
 WiFly wifly;
 
-const char mySSID[] = "Mirte's iPhone";
-const char myPassword[] = "helloworld";
+//const char mySSID[] = "Mirte's iPhone";
+const char mySSID[] = "mechanicalhuman";
+//const char myPassword[] = "helloworld";
+const char myPassword[] = "jorge2000";
 
-char server[] = "172.20.10.3";
+char server[] = "10.0.1.13";
 int port = 9001;
 
 boolean handshakeComplete = false;
 
-void setup()
-{
-    Serial.begin(115200);
+const static boolean GFORCE = true;
+const static boolean METERS = false;
 
-    Serial1.begin(9600);
-    if (!wifly.begin(&Serial1, &Serial)) {
-        Serial.println(F("Failed to start wifly"));
+// w/ MMA7361 accelerometer
+byte _sleepPin = 16;
+byte _xpin = A0;
+byte _ypin = A1;
+byte _zpin = A2;
+
+// w/ ADXL335 accelerometer
+//byte _xpin = A2;
+//byte _ypin = A3;
+//byte _zpin = A4;
+
+int _gc = 92; // gravity constant (inverted for proper sinage)
+
+int _0z = 660; // defaults to read values (this is how you would hardcode a calibration)
+int _0x = 660; // defaults to read values
+int _0y = 677; // defaults to reasonable values
+boolean _calibrated = false;
+
+void setup(){
+  Serial.begin(115200);
+  
+  pinMode(_sleepPin, OUTPUT); // output mode
+  pinMode(_xpin, INPUT); // input mode
+  pinMode(_ypin, INPUT); // input mode
+  pinMode(_zpin, INPUT); // input mode
+
+  digitalWrite(_sleepPin, HIGH); // turns off sleep mode and activates device
+  digitalWrite(_xpin, HIGH); // turn on pull up resistor
+  digitalWrite(_ypin, HIGH); // turn on pull up resistor
+  digitalWrite(_zpin, HIGH); // turn on pull up resistor
+  
+  calibrate();
+
+  Serial1.begin(9600);
+  if (!wifly.begin(&Serial1)) {
+      //Serial.println(F("Failed to start wifly"));
 	wifly.terminal();
-    }
+  }
 
-    /* Join wifi network if not already associated */
-    if (!wifly.isAssociated()) {
+  /* Join wifi network if not already associated */
+  if (!wifly.isAssociated()) {
 	Serial.println(F("Joining network"));
-        wifly.setSSID(mySSID);
-        wifly.setPassphrase(myPassword);
-        wifly.setJoin(WIFLY_WLAN_JOIN_AUTO);
-        wifly.enableDHCP();
-        wifly.setIpProtocol(WIFLY_PROTOCOL_TCP);
-        
+      wifly.setSSID(mySSID);
+      wifly.setPassphrase(myPassword);
+      wifly.setJoin(WIFLY_WLAN_JOIN_AUTO);
+      wifly.enableDHCP();
+      wifly.setIpProtocol(WIFLY_PROTOCOL_TCP);
+      
 	if (wifly.join(mySSID, myPassword, true)) {
 	    wifly.save();
 	    Serial.println(F("Joined wifi network"));
@@ -64,20 +85,17 @@ void setup()
 	    Serial.println(F("Failed to join wifi network"));
 	    wifly.terminal();
 	}
-    } else {
-        Serial.println(F("Already joined network"));
-    }
+  } else {
+      Serial.println(F("Already joined network"));
+  }
 
-    if (!connect(server, "connect", port)) {
+  if (!connect(server, "/", port)) {
 	Serial.print(F("Failed to connect to "));
 	Serial.println(server);
 	wifly.terminal();
-    } else {
-      Serial.println(F("Sending Hello World"));
-      send("Hello, World!");
-      handshakeComplete = true;
-    }
-
+  } else {
+    handshakeComplete = true;
+  }
 }
 
 char inBuf[128];
@@ -85,14 +103,14 @@ char outBuf[128];
 uint8_t outBufInd = 0;
 
 void loop() {
-    if (getMessage(inBuf, sizeof(inBuf)) > 0) {
+  if (getMessage(inBuf, sizeof(inBuf)) > 0) {
 	Serial.print(F("Received response: "));
 	Serial.println(inBuf);
-    }
+  }
 
-    if (Serial.available()) {
-        char ch = Serial.read();
-        if (ch == '\r') {
+  if (Serial.available()) {
+      char ch = Serial.read();
+      if (ch == '\r') {
 	    /* Got a carriage return, send the message */
 	    outBuf[outBufInd] = 0;	// null terminate the string
 	    send(outBuf);
@@ -103,70 +121,109 @@ void loop() {
 	    outBufInd++;
 	    Serial.write(ch);		// echo input back to Serial monitor
 	}
-    }
-    
-    if (handshakeComplete){
-//      char* str = "sending";
-       wifly.println(millis()%255);
-       delay(20);
-    }
+  }
+  
+  if (handshakeComplete){
+//     wifly.print("X");
+     wifly.print(convert(analogRead(_xpin), _0x), DEC);
+//     wifly.print(" Y");
+//     wifly.print(convert(analogRead(_ypin), _0y), DEC);
+//     wifly.print(" Z");
+//     wifly.println(convert(analogRead(_zpin), _0z), DEC);
+     delay(500);
+  }
 }
 
-/** See if there is a properly formatted message from the
- * server. A correct message starts with character 0, and ends
- * with character 255.
- * @param buf - buffer to store incoming message in
- * @param size - max size of message to store
- * @returns - size of the received message, or 0 if no message received
- */
 int getMessage(char *buf, int size){
-    int len = 0; 
-
-    if (wifly.available() > 0) {
-	if (wifly.read() == 0) {
-	    /* read up to the end of the message (255) */
-	    len = wifly.getsTerm(buf, size, 255);
-	}
+  int len = 0; 
+  
+  if (wifly.available() > 0) {
+    if (wifly.read() == 0) {
+      /* read up to the end of the message (255) */
+      len = wifly.getsTerm(buf, size, 255);
     }
-    return len;
+  }
+  return len;
 }
 
-/** Send a message to the server. */
 void send(const char *data) {
-    wifly.write((uint8_t)0);
-    wifly.write(data);
-    wifly.write((uint8_t)255);
+  wifly.write((uint8_t)0);
+  wifly.write(data);
+  wifly.write((uint8_t)255);
 }
 
-/** Connect to a websocket server. */
 bool connect(const char *hostname, const char *path, uint16_t port){
-    if (!wifly.open(hostname, port)) {
-        Serial.println(F("connect: failed to open TCP connection"));
+  if (!wifly.open(hostname, port)) {
+      Serial.println(F("connect: failed to open TCP connection"));
 	return false;
-    }
+  }
 
-    wifly.print(F("GET "));
-    wifly.print(path);
-    wifly.println(F(" HTTP/1.1"));
-    wifly.println(F("Upgrade: WebSocket"));
-    wifly.println(F("Sec-WebSocket-Key: bWlydGU=")); // base64 encoded something
-    wifly.println(F("Sec-WebSocket-Version: 13"));
-    wifly.println(F("Connection: Upgrade"));
-    wifly.print(F("Host: "));
-    wifly.println(hostname);
-    wifly.println(F("Origin:"));
-    wifly.println();
+  wifly.print(F("GET "));
+  wifly.print(path);
+  wifly.println(F(" HTTP/1.1"));
+  wifly.println(F("Upgrade: WebSocket"));
+  wifly.println(F("Sec-WebSocket-Key: bWlydGU="));
+  wifly.println(F("Sec-WebSocket-Version: 13"));
+  wifly.println(F("Connection: Upgrade"));
+  wifly.print(F("Host: "));
+  wifly.println(hostname);
+  wifly.println(F("Origin:"));
+  wifly.println();
 
-    /* Wait for the handshake response */
-    if (wifly.match(F("HTTP/1.1 101"), 30000)) {
-        Serial.println("received handshake from server");
-        wifly.println("handshake complete");
+  /* Wait for the handshake response */
+  if (wifly.match(F("HTTP/1.1 101"), 1000)) {
+      Serial.println("connect: received handshake from server");
+      wifly.println("connect: handshake complete");
 	wifly.flushRx(200);
 	return true;
-    }
+  }
+  
+  Serial.println(F("connect: handshake failed"));
+  wifly.close();
 
-    Serial.println(F("connect: handshake failed"));
-    wifly.close();
-
-    return false;
+  return true;
 }
+
+void calibrate(){
+  Serial.println("Please place device right-side up on a flat surface.");
+  delay(2000); //give user two seconds to comply.
+  Serial.println("Calibrating");
+  
+  _0z = readAverage(_zpin, 10) - _gc; // remove 1 G from the reading to get an accurate 0G
+  Serial.print("Z Axis Calibrated:"); Serial.println(_0z);
+  _0x = readAverage(_xpin, 10);
+  Serial.print("X Axis Calibrated:"); Serial.println(_0x);
+  _0y = readAverage(_ypin, 10);
+  Serial.print("Y Axis Calibrated:"); Serial.println(_0y);
+  
+  _calibrated = true;
+}
+
+int readAverage(int pin, int samples){
+  long readings = 0;
+  
+  for(int i = 0; i < samples; i++){
+    readings += analogRead(pin);
+  }
+
+  return (readings/samples);
+}
+
+float convert(float myReading, float my0G){
+  return convert(myReading, my0G, GFORCE); // defaults to G-Forces
+}
+
+float convert(float myReading, float my0G, bool myUnits){
+  float myResult=((my0G-myReading)/(_gc));
+  
+  if(myUnits)
+    return myResult; // G-Forces
+  else {
+    return (myResult * 9.80665);
+  }
+  
+// converts to m/s^2 using conventional value for gravity.
+// if you are on a different planet you may need to use a
+// value for your conversions.
+}
+
--- server.js
+++ server.js
@@ -1,37 +1,111 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
 var net = require('net');
+
+var sensorData;
+
+var message = {
+  "data" : ''
+}
+
+//Settings
+var HTTP_PORT = 9000;
+var NET_PORT = 9001;
+var WS_PORT = 9002;
+
+//Server
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"
+};
+
+http.createServer( function( req, res ) {
+
+    var fileToLoad;
+
+    if (req.url == '/') {
+        fileToLoad = 'index.html';
+    }
+    else {
+        fileToLoad = url.parse( req.url ).pathname.substr(1);
+    }
+
+    console.log( '[HTTP] :: Loading :: ' + 'frontend/' + fileToLoad );
+
+    var fileBytes;
+    var httpStatusCode = 200;
+
+    fs.exists( 'frontend/' + fileToLoad, function( doesItExist) {
+
+        if (!doesItExist) {
+            console.log( '[HTTP] :: Error loading :: ' + 'frontend/' + fileToLoad );
+
+            httpStatusCode = 404;
+        }
+
+        var fileBytes = fs.readFileSync( 'frontend/' + fileToLoad );
+
+        var mimeType = mimeTypes[path.extname(fileToLoad).split('.')[1]];
+
+        res.writeHead(httpStatusCode, {'Content-type':mimeType});
+        res.end(fileBytes);
+    });
+
+}).listen(HTTP_PORT);
+
 var socket;
- 
+var clients = [];
+
 var server = net.createServer(function(socket) {
-  socket.setEncoding('utf8');
-  
-  socket.on('connect', function() {
-    console.log(">>> received CONNECT");
-    console.log('WiFly connected with ip adress: ' + socket.remoteAddress);
+  socket.name = socket.remoteAddress + ":" + socket.remotePort;
+  clients.push(socket);
+
+  socket.write("HTTP/1.1 101", function(){
+    console.log('[CONN] New connection: ' + socket.name + ', total clients: ' + clients.length);
   });
-  
+
+  socket.setEncoding('utf8');
+    
   socket.on('error', function(data){
-    console.log("[EROR]");
     console.log(data);
   });
 
   socket.on('end', function() {
-    console.log('Disconnected');
+    console.log('[END] Disconnection: ' + socket.name + ', total clients: ' + clients.length);
   });
 
-  // socket.connect('connect', function(data){
-  //   console.log(">>> received CONNECT");
-  //   console.log("data: "+ data);
-  // })
-
   socket.on('data', function(data) {
-    console.log(">>> received DATA!");
-    console.log(socket.remoteAddress + " sends: " + data);
-    socket.write("HTTP/1.1 101", function(data){
-      console.log("something: "+data);
-    });
+    console.log('[RECV from ' + socket.remoteAddress + "] " + data);
+    message.data = data;
+    console.log('[SAVED] '+message.data);
   });
 });
  
-server.listen(9001, function() { //'listening' listener
-  console.log('start WiFly test server...');
+server.listen(NET_PORT, function() {
+  console.log("[INIT] Server running on port", NET_PORT);
 });
+
+var WebSocketServer = require('ws').Server
+  , wss = new WebSocketServer({port: WS_PORT});
+
+wss.on('connection', function(ws){
+  // ws.send(JSON.stringify(message));
+  setInterval(function () {
+    updateXData(ws)
+  }, 500);
+});
+
+function updateXData(ws){
+  var newMessage = {
+    "data": ""
+  }
+
+  newMessage.data = message.data
+  ws.send(JSON.stringify(newMessage));
+}
